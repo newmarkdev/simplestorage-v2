c12120d148d6c1cf8e95131c3e3d49e044741339
Shaun Newmark <shaun@Shauns-MacBook-Air.local>
2021-09-24 15:08:15 +0200

    final

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,8 +4,11 @@ pragma solidity >=0.4.21 <0.7.0;
 contract SimpleStorage {
   uint storedData;
 
+  event setEvent(uint newValue);
+
   function set(uint x) public {
     storedData = x;
+    emit setEvent(x);
   }
 
   function get() public view returns (uint) {
